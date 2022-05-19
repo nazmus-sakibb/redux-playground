16d9e81c45b0175227811090f254d365060cb2d7
NAZMUS SAKIB <nazmus.sakibbb1@gmail.com>
2022-05-20 02:14:53 +0600

    reducer

--- index.js
+++ index.js
@@ -33,12 +33,40 @@ const decrementCounter = () => {
 
 
 
-const addUser = () => {
-    return {
-        type: ADD_USER,
-        payload: {name: 'JS Redux'},
+// const addUser = () => {
+//     return {
+//         type: ADD_USER,
+//         payload: {name: 'JS Redux'},
+//     }
+// };
+
+
+
+// create reducer for counter
+const counterReducer = (state = initialCounterState, action) => {
+    
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+            //
+        
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+            //
+    
+        default:
+            state;
     }
-};
+}
+
+
+
 
 
 // 1. state
