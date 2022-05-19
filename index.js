2e959ab1042244f7d2c872e80b3340e7694b408d
NAZMUS SAKIB <nazmus.sakibbb1@gmail.com>
2022-05-20 00:57:02 +0600

    state declaration

---
+++ index.js
@@ -0,0 +1,19 @@
+// state
+const initialCounterState = {
+    count : 0
+}
+const initialUserState = {
+    users : [
+        {name : "Redux"},
+    ]
+}
+
+
+
+
+// Action -- object - type, payload
+// INCREMENT COUNTER
+{
+    type: 'INCREMENT',
+}
+// DECREMENT COUNTER
